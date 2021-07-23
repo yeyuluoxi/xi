63abaf7c2687bab50d5f149a0c4c15b8d47bbdc9
wangtao <1477017843@qq.com>
2021-07-23 18:02:23 +0800

    luo

--- src/Type/Page/PHome.d.ts
+++ src/Type/Page/PHome.d.ts
@@ -1,30 +1,6 @@
 import {RouterProps} from "react-router-dom";
 
-export interface HomeItem{
-  count: string,  //唯一标识
-  projectname: string, //项目名称
-  projectblock: string, //标段
-  submittime_manmade: string,  //交工时间(submittime)
-  projectlength_manmade: string, //标段长度
-  // projecttype: string,  //技术等级
-  techgrade_manmade: string,   //技术等级
-  privincecode_manmade: string, //省份
-  pactprice: string,    //中标价格
-  balanceprice: string,    //结算价格
-  source: string,    //来源
-  update: string,    //来源是否存在(update_backup)
-  webupdatetime: string,    //更新时间
-  RowKey: string,   //项目名称&行键
-  tablename: string, //表名
-  id: number
-}
-
 export interface HomeProp extends RouterProps{
-  val: number,
-  setVal: Function
-}
-
-export interface HomeTest{
   val: number,
   setVal: Function
 }
